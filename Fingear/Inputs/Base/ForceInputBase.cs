4931850ff5846ba8c89cc632886c96a4138a5cf2
Rémi Morillon <rmorillon@freneticexperience.com>
2017-05-26 23:25:20 +0200

    Refactor input bases

--- Fingear/Inputs/Base/ForceInputBase.cs
+++ Fingear/Inputs/Base/ForceInputBase.cs
@@ -3,29 +3,14 @@ using Fingear.Utils;
 
 namespace Fingear.Inputs.Base
 {
-    public abstract class ForceInputBase<TValue> : IForceInput<TValue>
+    public abstract class ForceInputBase<TValue> : InputBase<TValue>, IForceInput<TValue>
         where TValue : IEquatable<TValue>
     {
-        private TValue _value;
-        public abstract string DisplayName { get; }
-        public InputActivity Activity { get; private set; }
-        public abstract TValue Value { get; }
         public abstract TValue IdleValue { get; }
-        public abstract IInputSource Source { get; }
-        public bool Handled { get; private set; }
-        protected TValue LastValue { get; private set; }
 
-        public virtual void Update()
+        protected override InputActivity UpdateActivity(TValue value)
         {
-            Handled = false;
-            LastValue = _value;
-            _value = Value;
-            Activity = InputActivityUtils.UpdatePonctual(_value, LastValue, IdleValue);
-        }
-
-        public void Handle()
-        {
-            Handled = true;
+            return InputActivityUtils.UpdatePonctual(value, LastValue, IdleValue);
         }
     }
 }
---
+++ Fingear/Inputs/Base/InputBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Fingear.Utils;
+
+namespace Fingear.Inputs.Base
+{
+    public abstract class InputBase<TValue> : IInput<TValue>
+        where TValue : IEquatable<TValue>
+    {
+        private TValue _value;
+        public abstract string DisplayName { get; }
+        public InputActivity Activity { get; private set; }
+        public abstract TValue Value { get; }
+        public abstract IInputSource Source { get; }
+        public bool Handled { get; private set; }
+        protected TValue LastValue { get; private set; }
+
+        public virtual void Update()
+        {
+            Handled = false;
+            LastValue = _value;
+            _value = Value;
+            Activity = UpdateActivity(_value);
+        }
+
+        protected abstract InputActivity UpdateActivity(TValue value);
+
+        public void Handle()
+        {
+            Handled = true;
+        }
+    }
+}
