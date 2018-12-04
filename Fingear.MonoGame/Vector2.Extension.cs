f5e1eb2a6b60958ee0e31ce9ae2ce3b98ff9fb93
ReMinoer <rmorillon@freneticexperience.com>
2018-12-04 23:15:19 +0100

    Add IsActive extension method with vector conversion for Monogame

--- Fingear.MonoGame/Vector2.Extension.cs
+++ Fingear.MonoGame/Vector2.Extension.cs
@@ -2,6 +2,13 @@
 {
     static public class Vector2Extension
     {
+        static public bool IsActive(this IControl<System.Numerics.Vector2> control, out Microsoft.Xna.Framework.Vector2 value)
+        {
+            bool isActive = control.IsActive(out System.Numerics.Vector2 systemVector);
+            value = systemVector.AsMonoGameVector();
+            return isActive;
+        }
+
         static public Microsoft.Xna.Framework.Vector2 AsMonoGameVector(this System.Numerics.Vector2 vector2)
         {
             return new Microsoft.Xna.Framework.Vector2(vector2.X, vector2.Y);
