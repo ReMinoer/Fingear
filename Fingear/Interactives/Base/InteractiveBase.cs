34b90aa26f3ae7d1d83279b547002250c6021356
ReMinoer <rmorillon@freneticexperience.com>
2019-05-24 11:01:20 +0200

    Implement new components events

--- Fingear/Interactives/Base/InteractiveBase.cs
+++ Fingear/Interactives/Base/InteractiveBase.cs
@@ -50,5 +50,62 @@ namespace Fingear.Interactives.Base
         IEnumerable IComponent.Components => ((IComponent)ComponentImplementation).Components;
         IInteractive IComponent<IInteractive>.Parent => ((IComponent<IInteractive>)ComponentImplementation).Parent;
         IComponent IComponent.Parent => ((IComponent)ComponentImplementation).Parent;
+
+        private IComponent ComponentImplementationT0 => ComponentImplementation;
+        private IComponent<IInteractive> ComponentImplementationT1 => ComponentImplementation;
+
+        public event Event<IInteractiveContainer> ParentChanged
+        {
+            add => ComponentImplementation.ParentChanged += value;
+            remove => ComponentImplementation.ParentChanged -= value;
+        }
+
+        public event Event<IHierarchyChangedEventArgs<IInteractive, IInteractiveContainer>> HierarchyChanged
+        {
+            add => ComponentImplementation.HierarchyChanged += value;
+            remove => ComponentImplementation.HierarchyChanged -= value;
+        }
+
+        public event Event<IHierarchyComponentAddedEventArgs<IInteractive, IInteractiveContainer>> HierarchyComponentAdded
+        {
+            add => ComponentImplementation.HierarchyComponentAdded += value;
+            remove => ComponentImplementation.HierarchyComponentAdded -= value;
+        }
+
+        event Event<IComponent> IComponent.ParentChanged
+        {
+            add => ComponentImplementationT0.ParentChanged += value;
+            remove => ComponentImplementationT0.ParentChanged -= value;
+        }
+
+        event Event<IInteractive> IComponent<IInteractive>.ParentChanged
+        {
+            add => ComponentImplementationT1.ParentChanged += value;
+            remove => ComponentImplementationT1.ParentChanged -= value;
+        }
+
+        event Event<IHierarchyChangedEventArgs> IComponent.HierarchyChanged
+        {
+            add => ComponentImplementationT0.HierarchyChanged += value;
+            remove => ComponentImplementationT0.HierarchyChanged -= value;
+        }
+
+        event Event<IHierarchyChangedEventArgs<IInteractive>> IComponent<IInteractive>.HierarchyChanged
+        {
+            add => ComponentImplementationT1.HierarchyChanged += value;
+            remove => ComponentImplementationT1.HierarchyChanged -= value;
+        }
+
+        event Event<IHierarchyComponentAddedEventArgs> IComponent.HierarchyComponentAdded
+        {
+            add => ComponentImplementationT0.HierarchyComponentAdded += value;
+            remove => ComponentImplementationT0.HierarchyComponentAdded -= value;
+        }
+
+        event Event<IHierarchyComponentAddedEventArgs<IInteractive>> IComponent<IInteractive>.HierarchyComponentAdded
+        {
+            add => ComponentImplementationT1.HierarchyComponentAdded += value;
+            remove => ComponentImplementationT1.HierarchyComponentAdded -= value;
+        }
     }
 }
--- Fingear/Interactives/Base/InteractiveContainerBase.cs
+++ Fingear/Interactives/Base/InteractiveContainerBase.cs
@@ -24,7 +24,7 @@ namespace Fingear.Interactives.Base
                 interactive.Reset();
         }
 
-        bool IContainer<IInteractive>.Opened => ContainerImplementation.Opened;
+        bool IContainer.Opened => ContainerImplementation.Opened;
         IEnumerable<TComponent> IContainer<IInteractive, IInteractiveContainer, TComponent>.Components => ContainerImplementation.Components;
         void IContainer<IInteractive>.Link(IInteractive child) => ContainerImplementation.Link(child);
         void IContainer<IInteractive>.Unlink(IInteractive child) => ContainerImplementation.Unlink(child);
@@ -34,5 +34,26 @@ namespace Fingear.Interactives.Base
         void IContainer<IInteractive, IInteractiveContainer, TComponent>.Unlink(TComponent child) => ContainerImplementation.Unlink(child);
         bool IContainer<IInteractive, IInteractiveContainer, TComponent>.TryLink(TComponent child) => ContainerImplementation.TryLink(child);
         bool IContainer<IInteractive, IInteractiveContainer, TComponent>.TryUnlink(TComponent child) => ContainerImplementation.TryUnlink(child);
+
+        private IContainer ContainerImplementationT0 => ContainerImplementation;
+        private IContainer<IInteractive> ContainerImplementationT1 => ContainerImplementation;
+
+        public event Event<TComponent> ComponentAdded
+        {
+            add => ContainerImplementation.ComponentAdded += value;
+            remove => ContainerImplementation.ComponentAdded -= value;
+        }
+
+        event Event<IComponent> IContainer.ComponentAdded
+        {
+            add => ContainerImplementationT0.ComponentAdded += value;
+            remove => ContainerImplementationT0.ComponentAdded -= value;
+        }
+
+        event Event<IInteractive> IContainer<IInteractive>.ComponentAdded
+        {
+            add => ContainerImplementationT1.ComponentAdded += value;
+            remove => ContainerImplementationT1.ComponentAdded -= value;
+        }
     }
 }
